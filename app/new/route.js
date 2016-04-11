9ee62d78c3c1f87f145b3da354edea2c4f4dcf20
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-04-11 15:13:36 -0500

    Finish new hero controller

--- app/new/route.js
+++ app/new/route.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return fetch(`https://tiny-tn.herokuapp.com/collections/sj-heroes`)
+    .then((res) => res.json());
+  },
 });
